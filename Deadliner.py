874818fd62317832780b1f5523f3f57a1e5f31e5
LINETIA <45490649+LINETIA@users.noreply.github.com>
2020-05-16 07:06:01 +0900

    Add files via upload

---
+++ Deadliner.py
@@ -0,0 +1,30 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    # 「おはよう」で始まるか調べる
+    if message.content.startswith("おはよう"):
+        # 送り主がBotだった場合反応したくないので
+        if client.user != message.author:
+            # メッセージを書きます
+            m = "おはようございます" + message.author.name + "さん！"
+            # メッセージが送られてきたチャンネルへメッセージを送ります
+            await message.channel.send(m)
+    if message.content.startswith("hi") or message.content.startswith("Hi"):
+        # 送り主がBotだった場合反応したくないので
+        if client.user != message.author:
+            # メッセージを書きます
+            m = "おはようございます" + message.author.name + "さん！"
+            # メッセージが送られてきたチャンネルへメッセージを送ります
+            await message.channel.send(m)
+
+client.run("NzEwOTcwMjkwOTE3NzM2NDY4.Xr8OAw.vLYq9SwfRzmWwfxqXnCzugC2M6M")
